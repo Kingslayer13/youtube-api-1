c8ef325a72459438beded355ac92e2a9047b0579
Kingslayer <13kingslayer13@gmail.com>
2014-02-19 01:37:37 +0200

    All tests are passed. "On", "off" and "trigger" methods of player are working.

--- test/main.js
+++ test/main.js
@@ -1,31 +1,61 @@
 /**
  * Created by Kingslayer on 17.02.14.
  */
-define (['chai', 'youtube-api', 'jquery', 'load-player'], function(chai, api, $, player){
+define (['chai', 'youtube-api', 'jquery'], function(chai, api, $){
     var expect = chai.expect;
+    var player;
     describe('Youtube api', function(){
         it('should call ready event when youtube lib is loaded', function(done){
             expect(api.YoutubePlayer).to.be.a('undefined');
             api.load();
             api.on('ready', function(){
+                player = new api.YoutubePlayer('player', {
+                    width:   600,
+                    height:  400,
+                    videoId: 'yQlC6CfVC_g',
+                    wmode: 'transparent',
+                    playerVars: {
+                        rel: 0,
+                        wmode: 'transparent',
+                        modestbranding: 1,
+                        autohide: 1,
+                        showinfo: 0,
+                        controls: 1
+                    }
+                });
                 expect(api.YoutubePlayer).to.be.a('function');
-                done();
+                done()
             });
         });
 
         it('should be "iframe" element on the page ', function(){
             expect($('iframe').get(0).tagName.toLowerCase()).to.be.equal('iframe');
         });
-
-        it('should be "player" to be object', function(done){
-            api.on('ready', function(){
-                expect(player).to.be.a('object');
-                done();
-            });
-
-        })
     });
 
+    describe('Youtube events', function(){
+        it('should be div element created on player pause', function(done){
 
+            player.on('paused', function(e){ //event on pause
+                var exampleNode = '<div id="tested">'
+                $('#test').append(exampleNode);
+            });
+
+            player.trigger('paused');
+
+            expect($('#test')[0].children[0].id).to.be.equal('tested');
+            done();
+        });
+
+        it('should not add div element on player pause', function(done){
+
+            player.off('paused');
+
+            player.trigger('paused');
+
+            expect($('#test')[0].children.length).to.be.equal(1);
+            done();
+        });
+    });
 });
 
